70df1a4468cdebdef694af77f56284cb03f1b31e
jbryanp <jprase01@uw.edu>
2023-05-01 16:50:55 -0700

    convert importance graph to plotly and create json file

--- iBDawgsDashboard/correlation_heatmap.py
+++ iBDawgsDashboard/correlation_heatmap.py
@@ -3,9 +3,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 import statsmodels.formula.api as smf
 import seaborn as sns
+import plotly.figure_factory as ff
+import plotly.graph_objs as go
+
 
 def display_correlation_heat_map():
-    df = pd.read_csv("Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv")
+    df = pd.read_csv("./iBDawgsDashboard/Friday-WorkingHours-Afternoon-DDos.pcap_ISCX.csv")
     df.columns = df.columns.str.lstrip()
     df.columns = df.columns.str.replace(' ', '_')
     df.columns = df.columns.str.replace('/s', '_s')
@@ -13,6 +16,8 @@ def display_correlation_heat_map():
     df.replace([np.inf, -np.inf], np.nan, inplace=True)
     df.dropna(inplace=True)
     df["Target"] = np.where(df["Label"] == "BENIGN", 0, 1)
+    df["Target"] = df["Target"].astype(int)
+    df = df.drop('Label', axis=1)
 
     s = "Target ~ Flow_Duration + Total_Fwd_Packets + Total_Backward_Packets + Destination_Port"
     m = smf.logit(s, data = df).fit()
@@ -24,6 +29,13 @@ def display_correlation_heat_map():
 
     corr_matrix = corr_matrix.abs().sort_values(by=["Target"], ascending=False)
 
-    plt.figure(figsize=(20, 30))
-    sns.heatmap(corr_matrix[["Target"]], annot=True, cmap='coolwarm', linewidths=0.5)
-    plt.show()
+    x = corr_matrix.index.tolist()
+    z = corr_matrix.values.tolist()
+
+    heatmap = go.Heatmap(z=z, x=x, y=x, colorscale='Viridis')
+    data=[heatmap]
+    layout = go.Layout(title='Correlation Matrix', width=800, height=800)
+    fig = go.Figure(data=data, layout=layout)
+    fig.show()
+
+display_correlation_heat_map()
