002ba67f9581cb75898280e2ecdb4b3297c8f3e3
dgunadharma20 <dgunadharma20@gmail.com>
2023-05-11 17:58:05 -0700

    organize files

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -35,38 +35,3 @@ const App = () => {
 };
 
 export default App;
-
-// import Plot from 'react-plotly.js';
-
-// function App() {
-//   const [graphData, setGraphData] = useState({});
-
-//   useEffect(() => {
-//     // Fetch the graph data from the server
-//     fetch('/api')
-//       .then(response => response.json())
-//       .then(data => setGraphData(data))
-//       .catch(error => console.error(error));
-//   }, []);
-
-//   return (
-//     <div>
-//       <Plot
-//         data={[
-//           {
-//             x: [1, 2, 3],
-//             y: [2, 6, 3],
-//             type: 'scatter',
-//             mode: 'lines+markers',
-//             marker: {color: 'black'},
-//           },
-//           {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
-//         ]}
-//         layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
-//       />
-//       <Plot data={graphData.data} layout={graphData.layout} />
-//     </div>
-//   );
-// }
-
-// export default App;
